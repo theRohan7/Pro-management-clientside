48e1e18b54079253842dfc07f765ee1cb34dd8b0
Rohan sahu <therohansahu7@gmail.com>
2024-11-02 23:58:57 +0530

    added backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://localhost:8000/api/v1"
+// export const BACKEND_URL = "http://localhost:8000/api/v1"
+export const BACKEND_URL = 'https://pro-management-serverside-1.onrender.com/'
